bebf76a8c6831df328a5f7f2f1fb16c43941d940
AdrianPerez0306 <perezadrian.m@hotmail.com>
2024-12-25 15:32:16 -0300

    Starter commit

---
+++ src/components/aboutMe.tsx
@@ -0,0 +1,12 @@
+// import { NavLink, Outlet } from 'react-router-dom';
+// import { Header } from './header';
+
+// import { paths, PathTestId, pathToLabelMap } from './domain/routes';
+export const AboutMe = () => {
+
+    return <>
+        <div>ABOUT ME CONTENT</div>
+    </>
+};
+
+
