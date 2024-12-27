ca70130842587d2f19f4be639f476c1593a88042
AdrianPerez0306 <perezadrian.m@hotmail.com>
2024-12-27 03:25:21 -0300

    card de proyecto

--- src/components/cardProject/cardProject.tsx
+++ src/components/cardProject/cardProject.tsx
@@ -5,10 +5,17 @@ export const CardProject = () => {
 
     return <>
         <div className="cardProject">
-            <h5>TITLE</h5>
-            <img src="/asdsa" alt="IMG" />
-            <div className="technologys">UL LI Y DEMAS</div>
-            <p className="info">texto mucho</p>
+            <img src="./src/assets/readApp.jpeg" alt="IMG" />
+            <div className="content">
+                <h5>TITLE</h5>
+                <p className="info">texto mucho</p>
+            </div>
+            <div className="actions">
+                <button>Details</button>
+                <button>Github</button>
+                <button>Hosted</button>
+            </div>
+            {/* <div className="technologys">UL LI Y DEMAS</div> */}
         </div>
     </>
 };
